0b5b3a61bddb062a5aa9136a3d6144c8fec64afd
dlawjddn <jwl5015@naver.com>
2023-11-08 22:45:30 +0900

    Feat: login failure handler 생성

---
+++ security/src/main/java/io/basic/security/handler/LoginFailureHandler.java
@@ -0,0 +1,19 @@
+package io.basic.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Configuration
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        System.out.println("authentication is failed, msg: " + exception.getMessage());
+        response.sendRedirect("/login");
+    }
+}
