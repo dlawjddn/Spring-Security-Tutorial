5e5132825c7dcea585c02b02d2d53a668eeaf41d
dlawjddn <jwl5015@naver.com>
2023-11-08 23:54:14 +0900

    initial commit

---
+++ security-exercise/src/test/java/apply/security/SecurityExercise/SecurityExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package apply.security.SecurityExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
