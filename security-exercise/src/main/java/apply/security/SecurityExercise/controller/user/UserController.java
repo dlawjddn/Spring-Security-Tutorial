8b06ae3dd20b92d05d65ec8c12f6e4a5d6ee3412
dlawjddn <jwl5015@naver.com>
2023-11-10 23:16:54 +0900

    Feat: create user end-poing 추가

--- security-exercise/src/main/java/apply/security/SecurityExercise/controller/user/UserController.java
+++ security-exercise/src/main/java/apply/security/SecurityExercise/controller/user/UserController.java
@@ -1,12 +1,27 @@
 package apply.security.SecurityExercise.controller.user;
 
+import apply.security.SecurityExercise.dto.AccountDTO;
+import apply.security.SecurityExercise.service.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final AccountService accountService;
     @GetMapping("/mypage")
     public String myPage() throws Exception {
         return "user/mypage";
+    }
+    @GetMapping("/users")
+    public String createUser(){
+        return "user/login/register";
+    }
+    @PostMapping("/users")
+    public String createUser(AccountDTO accountDTO){
+        accountService.createAccount(accountDTO);
+        return "redirect:/";
     }
 }
