d6835293cf5e752db6fd9f315b494d73d3d9e7fd
dlawjddn <jwl5015@naver.com>
2023-11-10 23:22:51 +0900

    Feat: principle object 생성

---
+++ security-exercise/src/main/java/apply/security/SecurityExercise/security/service/AccountContext.java
@@ -0,0 +1,16 @@
+package apply.security.SecurityExercise.security.service;
+
+import apply.security.SecurityExercise.domain.Account;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+@Getter
+public class AccountContext extends User {
+    private final Account account;
+    public AccountContext(Account account,  Collection<? extends GrantedAuthority> authorities) {
+        super(account.getUsername(), account.getPassword(), authorities);
+        this.account = account;
+    }
+}
