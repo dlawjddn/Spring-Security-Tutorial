0c6f459a53f90a4d0fb2e8764b39794b0b8a0634
dlawjddn <jwl5015@naver.com>
2023-11-10 23:18:44 +0900

    Feat: account repository 생성

---
+++ security-exercise/src/main/java/apply/security/SecurityExercise/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package apply.security.SecurityExercise.repository;
+
+import apply.security.SecurityExercise.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
+}
