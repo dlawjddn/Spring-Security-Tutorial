d31340aec24f2b8a45143f0a223ca7eedda1a4cd
dlawjddn <jwl5015@naver.com>
2023-11-12 03:09:04 +0900

    Feat: user details service 생성

--- security-exercise/src/main/java/apply/security/SecurityExercise/security/service/CustomUserDetailsService.java
+++ security-exercise/src/main/java/apply/security/SecurityExercise/security/service/CustomUserDetailsService.java
@@ -4,6 +4,7 @@ import apply.security.SecurityExercise.domain.Account;
 import apply.security.SecurityExercise.repository.AccountRepository;
 import apply.security.SecurityExercise.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,7 +22,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Account> tempAccount = accountRepository.findByUsername(username);
-        if (tempAccount.isEmpty()) throw new UsernameNotFoundException("username not found");
+        if (tempAccount.isEmpty())
+            throw new UsernameNotFoundException("username not found");
         Account findAccount = tempAccount.get();
         ArrayList<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(findAccount.getRole()));
